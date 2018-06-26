3be4aa378a23bbd6a9859bbde9c763d4af2f1e1c
Wishwa Prabodha <ww46673@gmail.com>
2018-06-26 18:48:15 +0530

    files added manually

--- route.js
+++ route.js
@@ -1,32 +1,21 @@
 var express=require('express');
 var mysql = require('mysql');
 var bodyParser=require('body-parser');
-const mysqlssh = require('mysqlssh');
-const fs = require('fs');
 var app=express();
 
+// create application/json parser
+var jsonParser = bodyParser.json();
  
-mysqlssh.connect(
-    {
-        host: '129.154.102.73',
-        user: 'opc',
-        privateKey: fs.readFileSync(process.env.HOME + '/home/wishwa/Downloads/IPAY_MICROSERVICE_TESTING_PRIVATE_KEY.key')
-    },
-    {
-        host: '129.154.102.73',
-        user: 'root',
-        password : '',
-        database : 'nodetest'
-    }
-)
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'nodetest'
 });
-*/
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected !");
@@ -34,7 +23,7 @@ connection.connect(function(err) {
 
 
 app.set('view engine', 'pug');
-app.set('views', './views');
+//app.set('views', './views');
 
 app.get('/customer/:id', function(req, res){
     //connection.connect();
@@ -50,41 +39,41 @@ app.get('/customer/:id', function(req, res){
 app.get('/customer',function(req,res){
     //connection.connect();
     var customerList = [];
+    var i=0;
     var query='select * from customer';
     connection.query(query,function(err,results,f){
-        console.log(results);
-        for (var i in results) {
+    console.log(results);
+      for (i=0;i<5;i++) {
         // Create an object to save current row's data
-        
         var person = {
-            id:results[i].fid,
-            name:results[i].fname,
-            age:results[i].fage,
-            address:results[i].faddress
+            "id":results[i].id,
+            "name":results[i].name,
+            "age":results[i].age,
+            "address":results[i].address
                 };
         customerList.push(person);
-            }     
-        res.render('first_view', {"customer": customerList});
-
-    //res.end(JSON.stringify(results));   
+        }
+        //res.render('first_view', {person});
+        res.end(JSON.stringify(results));   
     //connection.end(); 
     });
 });
-
-app.post('/customers',function(req,res){
-    //connection.connect();
-    //res.render('first_view');
-    var query="insert into customer (id, name, age, address) values (14,"+"wishwa"+",23,"+"panadura"+");";
+app.post('/customer',jsonParser,function(req,res){
+    var id=req.body.id;
+    var name=req.body.name;
+    var age=req.body.age;
+    var address=req.body.address;
+    console.log("address: "+address);
+    var query="insert into customer values('"+id+"','"+name+"','"+
+    age+"','"+address+"');";
     connection.query(query,function(err,result,f){
-        if(err) throw err;
-        app.use(bodyParser.JSON);
-        //res.send(result);
-        res.end();
+        if(err) throw err;    
+         });
+
     });
-    //connection.end();
-});
 
 app.listen(3000,function(){
     console.log("Server Running!");
 });
+
 
